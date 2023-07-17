eecc543f3042e94c0ceb35fc12f25cb0a12551ca
Zach Bloss <zachbloss@rocketcentral.com>
2023-07-17 17:18:46 -0400

    improve readability

--- experiments.py
+++ experiments.py
@@ -1,32 +1,48 @@
 # Experiment framework
-import os
-import torch
-import numpy as np
-import statistics
 import operator
-from collections import Counter, defaultdict
-from tqdm import tqdm
+import os
+import pickle
 import random
+import statistics
+from collections import Counter, defaultdict
+from copy import deepcopy
 from functools import partial
 from itertools import repeat
-from copy import deepcopy
 from statistics import mode
-import pickle
-from sklearn.metrics.cluster import adjusted_rand_score, normalized_mutual_info_score
 
+import numpy as np
+import torch
+from sklearn.metrics.cluster import adjusted_rand_score, normalized_mutual_info_score
+from tqdm import tqdm
+
+from typing import Callable
+from compressors import DefaultCompressor
 
 class KnnExpText:
-    def __init__(self, agg_f, comp, dis):
-        self.aggregation_func = agg_f
-        self.compressor = comp
-        self.distance_func = dis
-        self.dis_matrix = []
+    def __init__(self, aggregation_function: Callable, compressor: DefaultCompressor, distance_function: Callable):
+        self.aggregation_func = aggregation_function
+        self.compressor = compressor
+        self.distance_func = distance_function
+        self.distance_matrix = []
 
-    def calc_dis(self, data, train_data=None, fast=False):
+    def calc_dis(self, data: list, train_data: list = None, fast: bool = False) -> None:
+        """
+        Calculates the distance between either `data` and itself or `data` and `train_data`
+        and appends the distance to `self.distance_matrix`.
+
+        Arguments:
+            data (list): Data to compute distance between.
+            train_data (list): [Optional] Training data to compute distance from `data`.
+            fast (bool): [Optional] Uses the _fast compression length function of `self.compressor`.
+
+        Returns:
+            None: None
+        """
+
+        data_to_compare = data
         if train_data is not None:
             data_to_compare = train_data
-        else:
-            data_to_compare = data
+
         for i, t1 in tqdm(enumerate(data)):
             distance4i = []
             if fast:
@@ -36,45 +52,116 @@ class KnnExpText:
             for j, t2 in enumerate(data_to_compare):
                 if fast:
                     t2_compressed = self.compressor.get_compressed_len_fast(t2)
-                    t1t2_compressed = self.compressor.get_compressed_len_fast(self.aggregation_func(t1,t2))
+                    t1t2_compressed = self.compressor.get_compressed_len_fast(
+                        self.aggregation_func(t1, t2)
+                    )
                 else:
                     t2_compressed = self.compressor.get_compressed_len(t2)
-                    t1t2_compressed = self.compressor.get_compressed_len(self.aggregation_func(t1, t2))
-                distance = self.distance_func(t1_compressed, t2_compressed, t1t2_compressed)
+                    t1t2_compressed = self.compressor.get_compressed_len(
+                        self.aggregation_func(t1, t2)
+                    )
+                distance = self.distance_func(
+                    t1_compressed, t2_compressed, t1t2_compressed
+                )
                 distance4i.append(distance)
-            self.dis_matrix.append(distance4i)
+            self.distance_matrix.append(distance4i)
 
-    def calc_dis_with_single_compressed_given(self, data, data_len=None, train_data=None):
+    def calc_dis_with_single_compressed_given(
+        self, data: list, data_len: list = None, train_data: list = None
+    ) -> None:
+        """
+        Calculates the distance between either `data`, `data_len`, or `train_data`
+        and appends the distance to `self.distance_matrix`.
+
+        Arguments:
+            data (list): Data to compute distance between.
+            train_data (list): [Optional] Training data to compute distance from `data`.
+            fast (bool): [Optional] Uses the _fast compression length function of `self.compressor`.
+
+        Returns:
+            None: None
+        """
+        
+        data_to_compare = data
         if train_data is not None:
             data_to_compare = train_data
-        else:
-            data_to_compare = data
+
         for i, t1 in tqdm(enumerate(data)):
             distance4i = []
-            t1_compressed = self.compressor.get_compressed_len_given_prob(t1, data_len[i])
+            t1_compressed = self.compressor.get_compressed_len_given_prob(
+                t1, data_len[i]
+            )
             for j, t2 in tqdm(enumerate(data_to_compare)):
-                t2_compressed = self.compressor.get_compressed_len_given_prob(t2, data_len[j])
-                t1t2_compressed = self.compressor.get_compressed_len(self.aggregation_func(t1, t2))
-                distance = self.distance_func(t1_compressed, t2_compressed, t1t2_compressed)
+                t2_compressed = self.compressor.get_compressed_len_given_prob(
+                    t2, data_len[j]
+                )
+                t1t2_compressed = self.compressor.get_compressed_len(
+                    self.aggregation_func(t1, t2)
+                )
+                distance = self.distance_func(
+                    t1_compressed, t2_compressed, t1t2_compressed
+                )
                 distance4i.append(distance)
-            self.dis_matrix.append(distance4i)
+            self.distance_matrix.append(distance4i)
 
-    def calc_dis_single(self, t1, t2):
+    def calc_dis_single(self, t1: str, t2: str) -> float:
+        """
+        Calculates the distance between `t1` and `t2` and returns 
+        that distance value as a float-like object.
+
+        Arguments:
+            t1 (str): Data 1.
+            t2 (str): Data 2.
+
+        Returns:
+            float-like: Distance between `t1` and `t2`.
+        """
+
         t1_compressed = self.compressor.get_compressed_len(t1)
         t2_compressed = self.compressor.get_compressed_len(t2)
-        t1t2_compressed = self.compressor.get_compressed_len(self.aggregation_func(t1, t2))
+        t1t2_compressed = self.compressor.get_compressed_len(
+            self.aggregation_func(t1, t2)
+        )
         distance = self.distance_func(t1_compressed, t2_compressed, t1t2_compressed)
         return distance
-    def calc_dis_single_multi(self, train_data, datum):
+
+    def calc_dis_single_multi(self, train_data: list, datum: str) -> list:
+        """
+        Calculates the distance between `train_data` and `datum` and returns 
+        that distance value as a float-like object.
+
+        Arguments:
+            train_data (list): Training data as a list-like object.
+            datum (str): Data to compare against `train_data`.
+
+        Returns:
+            list: Distance between `t1` and `t2`.
+        """
+
         distance4i = []
         t1_compressed = self.compressor.get_compressed_len(datum)
         for j, t2 in tqdm(enumerate(train_data)):
             t2_compressed = self.compressor.get_compressed_len(t2)
-            t1t2_compressed = self.compressor.get_compressed_len(self.aggregation_func(datum, t2))
+            t1t2_compressed = self.compressor.get_compressed_len(
+                self.aggregation_func(datum, t2)
+            )
             distance = self.distance_func(t1_compressed, t2_compressed, t1t2_compressed)
             distance4i.append(distance)
         return distance4i
-    def calc_dis_with_vector(self, data, train_data=None):
+
+    def calc_dis_with_vector(self, data: list, train_data: list = None):
+        """
+        Calculates the distance between `train_data` and `data` and returns 
+        that distance value as a float-like object.
+
+        Arguments:
+            train_data (list): Training data as a list-like object.
+            datum (str): Data to compare against `train_data`.
+
+        Returns:
+            float-like: Distance between `t1` and `t2`.
+        """
+
         if train_data is not None:
             data_to_compare = train_data
         else:
@@ -84,10 +171,27 @@ class KnnExpText:
             for j, t2 in enumerate(data_to_compare):
                 distance = self.distance_func(t1, t2)
                 distance4i.append(distance)
-            self.dis_matrix.append(distance4i)
-    def calc_acc(self, k, label, train_label=None, provided_distance_matrix=None, rand=False):
+            self.distance_matrix.append(distance4i)
+
+    def calc_acc(
+        self, k: int, label: str, train_label: str = None, provided_distance_matrix: list = None, rand: bool = False
+    ):
+        """
+        Calculates the accuracy of the algorithm.
+
+        Arguments:
+            k (int?): TODO
+            label (str): Predicted Label.
+            train_label (str): Correct Label.
+            provided_distance_matrix (list): Calculated Distance Matrix to use instead of `self.distance_matrix`.
+            rand (bool): TODO
+
+        Returns:
+            tuple: predictions, and list of bools indicating prediction correctness. 
+
+        """
         if provided_distance_matrix is not None:
-            self.dis_matrix = provided_distance_matrix
+            self.distance_matrix = provided_distance_matrix
         correct = []
         pred = []
         if train_label is not None:
@@ -97,14 +201,17 @@ class KnnExpText:
         else:
             compare_label = label
             start = 1
-            end = k+1
-        for i in range(len(self.dis_matrix)):
-            sorted_idx = np.argsort(np.array(self.dis_matrix[i]))
+            end = k + 1
+
+        for i in range(len(self.distance_matrix)):
+            sorted_idx = np.argsort(np.array(self.distance_matrix[i]))
             pred_labels = defaultdict(int)
             for j in range(start, end):
                 pred_l = compare_label[sorted_idx[j]]
                 pred_labels[pred_l] += 1
-            sorted_pred_lab = sorted(pred_labels.items(), key=operator.itemgetter(1), reverse=True)
+            sorted_pred_lab = sorted(
+                pred_labels.items(), key=operator.itemgetter(1), reverse=True
+            )
             most_count = sorted_pred_lab[0][1]
             if_right = 0
             most_label = sorted_pred_lab[0][0]
@@ -120,11 +227,12 @@ class KnnExpText:
                     most_voted_labels.append(pair[0])
             if rand:
                 most_label = random.choice(most_voted_labels)
-                if_right = 1 if most_label==label[i] else 0
+                if_right = 1 if most_label == label[i] else 0
             pred.append(most_label)
             correct.append(if_right)
-        print("Accuracy is {}".format(sum(correct)/len(correct)))
+        print("Accuracy is {}".format(sum(correct) / len(correct)))
         return pred, correct
+
     def combine_dis_acc(self, k, data, label, train_data=None, train_label=None):
         correct = []
         pred = []
@@ -135,7 +243,7 @@ class KnnExpText:
         else:
             compare_label = label
             start = 1
-            end = k+1
+            end = k + 1
         if train_data is not None:
             data_to_compare = train_data
         else:
@@ -147,7 +255,9 @@ class KnnExpText:
             for j in range(start, end):
                 pred_l = compare_label[sorted_idx[j]]
                 pred_labels[pred_l] += 1
-            sorted_pred_lab = sorted(pred_labels.items(), key=operator.itemgetter(1), reverse=True)
+            sorted_pred_lab = sorted(
+                pred_labels.items(), key=operator.itemgetter(1), reverse=True
+            )
             most_count = sorted_pred_lab[0][1]
             if_right = 0
             most_label = sorted_pred_lab[0][0]
@@ -170,7 +280,9 @@ class KnnExpText:
         for j in range(k):
             pred_l = train_label[sorted_idx[j]]
             pred_labels[pred_l] += 1
-        sorted_pred_lab = sorted(pred_labels.items(), key=operator.itemgetter(1), reverse=True)
+        sorted_pred_lab = sorted(
+            pred_labels.items(), key=operator.itemgetter(1), reverse=True
+        )
         most_count = sorted_pred_lab[0][1]
         if_right = 0
         most_label = sorted_pred_lab[0][0]
@@ -180,8 +292,6 @@ class KnnExpText:
             if pair[0] == label:
                 if_right = 1
                 most_label = pair[0]
-        pred=most_label
-        correct=if_right
+        pred = most_label
+        correct = if_right
         return pred, correct
-
-
