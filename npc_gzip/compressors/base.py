5b404968105e5341b1700a33f7b6d39b5a9962a6
Zach Bloss <zacharybloss@gmail.com>
2023-07-26 17:05:07 -0400

    add helper function to open files
    add BaseCompressor as acceptable compressor type

--- npc_gzip/compressors/base.py
+++ npc_gzip/compressors/base.py
@@ -1,3 +1,4 @@
+import os
 from types import ModuleType
 
 from npc_gzip.exceptions import InvalidCompressorException
@@ -11,7 +12,40 @@ class BaseCompressor:
     """
 
     def __init__(self, compressor: ModuleType):
+
+        if not isinstance(compressor, (ModuleType, BaseCompressor)):
+            raise InvalidCompressorException(f"Not a function passed: {type(compressor)}")
         self.compressor = compressor
+
+    def _open_file(self, filepath: str, as_bytes: bool = False):
+        """
+        Helper function that loads and returns the contents
+        of a file at `filepath`. Optional `as_bytes` parameter
+        will load the file contents with `open(filepath, 'rb')`
+        if True.
+
+        Arguments:
+            filepath (str): Path to the file to read contents from.
+            as_bytes (bool): [Optional] If true, opens the file as bytes.
+
+        Returns:
+            str: File contents as a string.
+        """
+
+        assert os.path.isfile(filepath), f"Filepath ({filepath}) does not exist."
+        file_contents = None
+        open_mode = "r"
+        if as_bytes:
+            open_mode = "rb"
+
+        with open(filepath, open_mode) as f:
+            file_contents = f.read()
+            f.close()
+
+        if as_bytes:
+            file_contents = file_contents.decode("utf-8")
+
+        return file_contents
 
     def _compress(self, x: str) -> bytes:
         """
@@ -26,8 +60,8 @@ class BaseCompressor:
         """
 
         x = str(x)
-        x = x.encode("utf-8")
-        compressed = self.compressor.compress(x)
+        x: bytes = x.encode("utf-8")
+        compressed: bytes = self.compressor.compress(x)
         return compressed
 
     def get_compressed_length(self, x: str) -> int:
@@ -59,6 +93,7 @@ class BaseCompressor:
                  characters in `x`.
         """
 
+        x = str(x)
         compressed_value: bytes = self._compress(x)
         compressed_length: int = len(compressed_value)
         compressed_length_in_bits: int = compressed_length * 8
@@ -79,5 +114,3 @@ if __name__ == "__main__":
     example = "Hello there!"
     compressed_length: int = compressor.get_compressed_length(example)
     bits_per_character: float = compressor.get_bits_per_character(example)
-
-    print(compressed_length, bits_per_character)
