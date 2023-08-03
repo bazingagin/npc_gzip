fdfe6f377545fc6e1fc0afcd8d4dce57333830b6
Eliah Kagan <degeneracypressure@gmail.com>
2023-08-03 18:28:41 +0000

    Put upper bound back to 50
    This puts the upper bound for the randomly determined sample size
    back to 50. The immediately preceding commits verified that the
    bug can be fixed by a change in the lower bound, but we don't
    always want to use a lower bound of 2!
    
    It may be best to choose the sample size non-randomly, perhaps
    testing multiple sample sizes per run using parametrization or
    subtests. But that is more extensive than the small change needed
    for #45, and thus beyond the scope here.

--- tests/test_knn_classifier.py
+++ tests/test_knn_classifier.py
@@ -116,7 +116,7 @@ class TestKnnClassifier:
         assert labels.shape == (1,)
         assert similar_samples.shape == (top_k, 1)
 
-        test_set_size = random.randint(2, 2)  # FIXME: Put upper bound back to 50.
+        test_set_size = random.randint(2, 50)
         test_set = [self.sample_input for _ in range(test_set_size)]
         top_k = 2
         (distance, labels, similar_samples) = self.model.predict(test_set, top_k)
